e7569f3efa23799dc26a45dd9cf9df4137b7b6a0
Sami Alakus <sami@boutique.tech>
2023-12-22 12:23:02 +1100

    feat: Set to use watermill (incomplete)

--- subscriber/kafka/kafka.go
+++ subscriber/kafka/kafka.go
@@ -3,26 +3,46 @@ package kafka
 import (
 	"context"
 	"fmt"
+	"time"
 
-	kafka "github.com/segmentio/kafka-go"
+	"github.com/Shopify/sarama"
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill-kafka/v2/pkg/kafka"
+	"github.com/ThreeDotsLabs/watermill/message"
 )
 
 var topic = "test-topic"
 var brokerAddress = "kafka:9092"
 
-var KafkaReader = kafka.NewReader(kafka.ReaderConfig{
-	Brokers: []string{brokerAddress},
-	Topic:   topic,
-})
+func CreateKafkaSubscriber(brokers, consumerGroup string) (*kafka.Subscriber, error) {
+	saramaSubscriberConfig := kafka.DefaultSaramaSubscriberConfig()
+	saramaSubscriberConfig.Consumer.Offsets.Initial = sarama.OffsetOldest
+	saramaSubscriberConfig.Consumer.Group.Heartbeat.Interval = 3 * time.Second
 
-func Listen(ctx context.Context, cb func(msg string)) {
+	return kafka.NewSubscriber(
+		kafka.SubscriberConfig{
+			Brokers:               []string{brokers},
+			Unmarshaler:           kafka.DefaultMarshaler{},
+			OverwriteSaramaConfig: saramaSubscriberConfig,
+			ConsumerGroup:         consumerGroup,
+		},
+		watermill.NewStdLogger(false, false),
+	)
+}
+
+// Listens for messages from Kafka and sends them to a callback function
+func Listen(ctx context.Context, messages <-chan *message.Message, cb func(msg string)) {
+
 	for {
-		message, err := KafkaReader.ReadMessage(ctx)
-		if err != nil {
-			fmt.Println("Could not read message", err)
+		select {
+		case <-ctx.Done():
+			return // Context canceled, exit the function
+		case message, ok := <-messages:
+			if !ok {
+				return // Channel closed, exit the function
+			}
+			fmt.Println("Received from Kafka:", string(message.Payload))
+			cb(string(message.Payload))
 		}
-
-		fmt.Println("Received from Kafka: ", string(message.Value))
-		cb(string(message.Value))
 	}
 }
--- subscriber/main.go
+++ subscriber/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/sami-oneonetwo/go-microservices/subscriber/connections"
@@ -10,10 +11,26 @@ import (
 	"github.com/sami-oneonetwo/go-microservices/subscriber/messages"
 )
 
+const (
+	port              = ":8889"
+	kafkaBrokers      = "kafka:9092"
+	consumerGroupName = "test_consumer_group"
+	topic             = "test-topic"
+)
+
 func main() {
 	fmt.Println("Setting up server in 8889")
 	http.HandleFunc("/", connections.Upgrade)
+
+	// Create Kafka subscriber
+	kafkaSubscriber, err := kafka.CreateKafkaSubscriber(kafkaBrokers, consumerGroupName)
+	if err != nil {
+		log.Fatalf("Error creating Kafka subscriber: %v", err)
+	}
+
 	ctx := context.Background()
-	go kafka.Listen(ctx, messages.SendMessage)
+	kafkaMessages, err := kafkaSubscriber.Subscribe(ctx, topic)
+
+	go kafka.Listen(ctx, kafkaMessages, messages.SendMessage)
 	http.ListenAndServe(":8889", nil)
 }
